408323ad0d1114b5b1cde42c34dde2bbee7baae3
Jumpmane <idopozner10@gmail.com>
2024-05-21 21:04:55 +0300

    semi functional - no server access

--- player-client/src/components/MainPage.tsx
+++ player-client/src/components/MainPage.tsx
@@ -1,11 +1,20 @@
-import React from "react";
-import { Button, IconButton, TextField, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  IconButton,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 import MicIcon from "@mui/icons-material/Mic";
 
 const MainPage = () => {
+  const [answer, setAnswer] = useState<string>("");
+
+  // sets the speech recognition to only recognize english
   SpeechRecognition.getRecognition().lang = "en-US";
 
   const {
@@ -15,27 +24,41 @@ const MainPage = () => {
     browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
 
+  useEffect(() => {
+    setAnswer(transcript);
+  }, [transcript]);
+
+  const submitGuess = () => {
+    // TODO: send guess to server
+  };
+
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
 
   return (
-    <>
+    <Stack width="95%" alignItems={"center"}>
       <Typography>Guess The Song</Typography>
       <IconButton
         size="large"
-        color="primary"
-        onClick={() => SpeechRecognition.startListening()}
+        color={listening ? "error" : "primary"}
+        onClick={() =>
+          !listening
+            ? SpeechRecognition.startListening()
+            : SpeechRecognition.stopListening()
+        }
       >
         <MicIcon />
       </IconButton>
       <p>Microphone: {listening ? "on" : "off"}</p>
-      <Button onClick={SpeechRecognition.stopListening}>Stop</Button>
-      <Button onClick={resetTranscript}>Reset</Button>
-      <p>{transcript}</p>
-      <TextField value={transcript} />
-      <Button>Submit</Button>
-    </>
+      <TextField
+        value={answer}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+          setAnswer(event.target.value);
+        }}
+      />
+      <Button onClick={submitGuess}>Submit</Button>
+    </Stack>
   );
 };
 
