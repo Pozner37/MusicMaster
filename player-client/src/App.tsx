98ab605a9bb6d8075895f580a678a3362965b9b8
Jumpmane <idopozner10@gmail.com>
2024-06-30 15:58:41 +0300

    starting to connect to server side

--- player-client/src/App.tsx
+++ player-client/src/App.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./components/Login";
 import MainPage from "./components/MainPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "main",
+    element: <MainPage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ player-client/src/components/Login.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Button, Stack, TextField, Typography } from "@mui/material";
+import axios from "axios";
+import { redirect } from "react-router-dom";
+
+const Login = () => {
+  const [gameId, setGameId] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+
+  const enterGame = () => {
+    axios
+      .get(
+        `http://localhost:4000/join-game?gameId=${gameId}&userName=${username}`,
+      )
+      .then(() => {
+        redirect(`/main`);
+      });
+  };
+
+  return (
+    <Stack width="95%" alignItems={"center"}>
+      <Typography variant={"h4"}>MusicMaster</Typography>
+      <TextField
+        value={gameId}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+          setGameId(event.target.value);
+        }}
+        placeholder={"Enter PIN"}
+      />
+      <TextField
+        value={username}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+          setUsername(event.target.value);
+        }}
+        placeholder={"Enter name"}
+      />
+      <Button onClick={enterGame}>Submit</Button>
+    </Stack>
+  );
+};
+
+export default Login;
