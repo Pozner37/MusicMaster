cb65837ed938110955650e64dfdc9a6501201908
Jumpmane <idopozner10@gmail.com>
2024-08-25 11:47:01 +0300

    bug fixes

--- src/components/Buzzer.tsx
+++ src/components/Buzzer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Stack, Typography } from "@mui/material";
 import { socket } from "../socket";
 import { useNavigate } from "react-router-dom";
