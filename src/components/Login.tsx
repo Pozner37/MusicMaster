22d4ec524f930931db97266fe6ce36a37a11c5a5
Oren <oren.hta@gmail.com>
2024-08-31 16:42:12 +0300

    add query param gameId

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { Button, Card, Stack, TextField } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { socket } from "../socket";
 
 const Login = () => {
-  const [gameId, setGameId] = useState<string>("");
+  const [searchParams] = useSearchParams();
+  const queryParamGameId = searchParams.get("gameId");
+  const [gameId, setGameId] = useState<string>(queryParamGameId || "");
   const [localUsername, setLocalUsername] = useState<string>("");
 
   const navigate = useNavigate();
@@ -35,15 +37,17 @@ const Login = () => {
       </div>
       <Card>
         <Stack alignItems={"center"} spacing={2} padding={3}>
-          <TextField
-            value={gameId}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setGameId(event.target.value);
-            }}
-            placeholder={"Enter PIN"}
-            variant="standard"
-            inputProps={{ inputMode: "numeric", maxLength: 6 }}
-          />
+          {!queryParamGameId && (
+            <TextField
+              value={gameId}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setGameId(event.target.value);
+              }}
+              placeholder={"Enter PIN"}
+              variant="standard"
+              inputProps={{ inputMode: "numeric", maxLength: 6 }}
+            />
+          )}
           <TextField
             value={localUsername}
             onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
